a9fbedcea96c15eb9b4f4c66b7748cb7f9b48aee
Marcelo Filipov <marcelo.filipov@gmail.com>
2020-07-11 03:15:54 -0300

    Implemented pagination, tooltip in buttons and responsive table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
 
 import { TabViewModule } from 'primeng/tabview';
 
@@ -19,6 +20,7 @@ import { AppComponent } from './app.component';
     InputTextModule,
     ButtonModule,
     TableModule,
+    TooltipModule,
     TabViewModule,
     AppRoutingModule
   ],
